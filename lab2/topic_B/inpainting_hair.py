f4b051dbf08bd679586d50b5d542753e0d2dcb72
Hbrtjm <hubertm@remitly.com>
2026-04-08 23:56:14 +0200

    Lab 2 work in progress

---
+++ lab2/topic_B/inpainting_hair.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+
+SEG_PARAMS = dict(
+    b_minus_r_thresh = 30,
+    b_minus_g_thresh = 20,
+    b_min_val        = 80,
+    dilate_k         = 4,
+    dilate_iter      = 1,
+)
+
+def define_mask():
+    pass
+
